78d0813a07025353faca69438bbb0e910005b04d
Britton Stanfill <britton.stanfill@gmail.com>
2013-11-06 22:51:37 -0700

    trying to get a database working

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var dbName = 'stacktrack';
+var dbName = 'framework_info';
 var dbUser = 'root';
-var dbPassword = null;
+var dbPassword = 'root';
 
 
 
@@ -13,27 +13,27 @@ var sequelize = new Sequelize(dbName, dbUser, dbPassword, {
 	"host":"localhost"
 });  //creating an instance of the sequelize object.
 
-var framework = sequelize.define('framework_data',{
+var frameWork = sequelize.define('framework_test', {
 	name: Sequelize.STRING,
-	question_number: Sequelize.INTEGER,
-	forks_count: Sequelize.INTEGER,
-	watchers_count: Sequelize.INTEGER,
-	updated_at: Sequelize.DATE
-})
+	fork_count: Sequelize.INTEGER
+},
+{
+	freezeTableName:true
+});
 
 
 sequelize.sync();
 
 
-app.configure(function(){
-	app.use(express.bodyParser());
-	app.use(function(response, request, next){
-		response.header('Access-Control-Allow-Origin', '*');
-		response.header('Access-Control-Allow-Methods','Options');
-		response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-		next();
-	});
-});
+// app.configure(function(){
+// 	app.use(express.bodyParser());
+// 	app.use(function(response, request, next){
+// 		response.header('Access-Control-Allow-Origin', '*');
+// 		response.header('Access-Control-Allow-Methods','Options');
+// 		response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 		next();
+// 	});
+// });
 
 	// GET Requests 
 
