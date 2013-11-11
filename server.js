9e82015a925d0762297241bf39dc3e1f47407747
Britton Stanfill <britton.stanfill@gmail.com>
2013-11-10 23:11:20 -0700

    Adding API stuff

--- server.js
+++ server.js
@@ -1,46 +1,79 @@
 var Sequelize = require('sequelize');
 var express = require('express');
+var https = require('https');
+var http = require("http");
+
 var app = express();
+
+var clientApi = require('my_modules/clientApi');
+var serverApi = require('my_modules/serverApi');
+
+
+
 var port = 3000;
 
 var dbName = 'frametest';
 var dbUser = 'root';
-var dbPassword = 'root';
-
-
+var dbPassword = null;
 
 var sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-	"host":"localhost"
+	 host:'localhost',
+	 dialect:'mysql'
 });  //creating an instance of the sequelize object.
 
-var Framework = sequelize.define('framework_test', {
+var Framework = sequelize.define('Framework', {
 	name: Sequelize.STRING,
-	fork_count: Sequelize.INTEGER
+	fork_count: Sequelize.INTEGER,
+	references: Sequelize.INTEGER
 },
 {
 	freezeTableName:true
 });
 
-var testframe = new Framework({
-	name: 'jQuery',
-	fork_count:'105232'
-})
-
+Framework.create({
+	name:'test',
+	fork_count:1021,
+	references:12412
+});
 
 sequelize.sync();
 
 
-// app.configure(function(){
-// 	app.use(express.bodyParser());
-// 	app.use(function(response, request, next){
-// 		response.header('Access-Control-Allow-Origin', '*');
-// 		response.header('Access-Control-Allow-Methods','Options');
-// 		response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-// 		next();
+	// I have just recently read that this is not needed if you are using express
+	// app.configure(function(){
+	// 	app.use(express.bodyParser());
+	// 	app.use(function(response, request, next){
+	// 		response.header('Access-Control-Allow-Origin', '*');
+	// 		response.header('Access-Control-Allow-Methods','Options');
+	// 		response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	// 		next();
+	// 	});
+	// });
+
+
+// app.get('/', function(req, res){
+
+// 	https.get("https://api.github.com/search/repositories?q=angular&sort=stars&order=desc", function(res) {
+// 	  console.log("Got response: " + res.statusCode);
+// 	  var apiResponse = res;
+
+// 	  console.log(apiResponse);
+
+// 		}).on('error', function(e) {
+// 	  	console.log("Got error: " + e.messag);
 // 	});
+
+//     res.send('completed get request');
+//     console.log('are we hitting get');
 // });
 
-	// GET Requests 
+
+
+clientApi();
+serverApi();
+
+
+
 
 app.listen(port);
 console.log('Server running at ' + port);
