1517707a13384edfa02ebf150b6deca34db8a0a6
Britton Stanfill <britton.stanfill@gmail.com>
2013-11-12 19:19:41 -0700

    Adjusting API Call

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var request = require('request');
 
 // var https = require('https');
-// var http = require("http");
+// var http = require("http");  // I am now using request to handle these instead.
 
 
 var app = express();
@@ -47,34 +47,36 @@ app.use(express.bodyParser());
 app.get('/', function(req, res){
 
 	request("https://api.github.com/search/repositories?q=forks:>=5000", function(error, response, body) {
-	  var apiResponse = body;
-	  console.log("Got response: " + body);
+		var apiResponse = JSON.parse(body);
+		console.log(apiResponse.total_count);
+
+
 	  // var apiResponse = res;
 	  // console.log(res);
 
 
 
+ 
+		var output = '';
 
-		// var output = '';
-
-		// for (var i=0; i<apiResponse['items'].length; i++){
-		//     var bit = apiResponse['items'][i];
-		//     output += '[name: "' + bit['name'] +
-		//         '", updated: "' + bit['updated_at'] +
-		//         '", fork: "' + bit['forks_count'] +
-		//         ']\n';
-		// };
-		// alert(output);
-
-
+		for (var i=0; i<apiResponse['items'].length; i++){
+		    var bit = apiResponse['items'][i];
+		    output += '[name: "' + bit['name'] +
+		        '", updated: "' + bit['updated_at'] +
+		        '", fork: "' + bit['forks_count'] +
+		        ']\n';
+		};
+		console.log(output);
 
 
-		}).on('error', function(e) {
-	  	console.log("Got error: " + e.messag);
+
+
+		// }).on('error', function(e) {
+	 //  	console.log("Got error: " + e.messag);
 	});
 
-    res.send('completed get request');
-    console.log('we hitting get');
+    // res.send('completed get request');
+    // console.log('we hitting get');
 });
 
 
