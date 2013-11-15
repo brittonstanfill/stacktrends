7f29ca9244bfcba19eeedc04bbba6f511566676e
Britton Stanfill <britton.stanfill@gmail.com>
2013-11-15 16:50:45 -0700

    It is now outputting into index

--- public/stackTrendController.js
+++ public/stackTrendController.js
@@ -12,11 +12,21 @@ app.controller('stackTrendController', function($scope, $http, $resource){
 
 // }
 
-	// $http.defaults.useXDomain = true;
-	var framework_forks = $resource('http://localhost:3000/api-forks');
-    $scope.github_framework_forks = framework_forks.query(); 
-    console.log($scope.github_framework_forks);
+ var framework_forks = $resource('http://localhost:3000/api-forks');
 
+    var forks = framework_forks.query(function() {
+        $scope.framework_forks = forks;
+        
+        $scope.apply();
+    });
+
+
+
+	// $http.defaults.useXDomain = true;
+	// var framework_forks = $resource('http://localhost:3000/api-forks');
+ //    $scope.github_framework_forks = framework_forks.query(); 
+ //    console.log($scope.github_framework_forks);
+	// framework_forks.query(function() {console.log(tests);});
 	// $scope.framework_forks = framework_forks;
 	// $scope.$apply();
 });
